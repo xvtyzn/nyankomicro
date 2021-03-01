fb92d8d66e49ac129e67ffb2520a8c215bf51269
xvtyzn <ide_0109@outlook.jp>
2021-03-01 18:27:59 +0900

    variable update

--- R/deseq2_tree.R
+++ R/deseq2_tree.R
@@ -21,7 +21,7 @@
 #' @export
 #'
 deseq2_tree <- function(physeq, deseq2_results, level = c("Kingdom", "Phylum",
-    "Class", "Order", "Family", "Genus", "Species"), alpha = 0.05, sample_annotation = Status) {
+    "Class", "Order", "Family", "Genus", "Species"), alpha = 0.05, sample_annotation = "Status") {
     sig_asv <- deseq2_results %>% as.data.frame() %>% dplyr::filter(padj < alpha)
 
     sig_tax <- physeq %>% tax_table() %>% as.data.frame() %>% rownames_to_column("ASV") %>%
@@ -39,12 +39,12 @@ deseq2_tree <- function(physeq, deseq2_results, level = c("Kingdom", "Phylum",
     tt <- cbind(log(sig_rate_otu + 1), sample_data(physeq_sig)[, 1]) %>% rownames_to_column("Sample") %>%
         pivot_longer(c(-Sample, -Status), names_to = "ASV", values_to = "read")
 
-    gh <- tt %>% ggplot() + geom_tile(aes(x = Sample, y = ASV, fill = sample_annotation,
+    gh <- tt %>% ggplot() + geom_tile(aes(x = Sample, y = ASV, fill = ~get(sample_annotation),
         alpha = read)) + scale_alpha_continuous(aes(show.legend = Relative_Abundance)) +
         ylab(NULL) + theme_minimal() + theme(axis.text.x = element_text(angle = 45,
         hjust = 1), axis.text.y = element_blank(), axis.ticks.y = element_blank())
 
-    fc_gg <- sigtab %>% ggplot(aes(y = ASV, x = log2FoldChange, color = level)) +
+    fc_gg <- sigtab %>% ggplot(aes(y = ASV, x = log2FoldChange, color = ~get(level))) +
         geom_point(size = 3) + geom_segment(aes(x = 0, xend = log2FoldChange,
         y = ASV, yend = ASV), color = "grey") + theme(axis.text.x = element_text(angle = -90,
         hjust = 0, vjust = 0.5)) + ylab(NULL) + theme_minimal() + theme(axis.text.x = element_text(angle = 90,
@@ -52,7 +52,7 @@ deseq2_tree <- function(physeq, deseq2_results, level = c("Kingdom", "Phylum",
         scale_color_simpsons() + theme(legend.position = "none")
 
     p_gg <- sigtab %>% mutate(log2padj = -log2(padj)) %>% ggplot(aes(y = ASV,
-        x = log2padj, fill = level)) + geom_bar(stat = "identity") + ylab(NULL) +
+        x = log2padj, fill = ~get(level))) + geom_bar(stat = "identity") + ylab(NULL) +
         theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 0),
         axis.text.y = element_blank(), legend.text = element_text(face = "italic")) +
         scale_fill_simpsons()
