749bda1f7167fc72cb8fddb57a935798bfb55162
xvtyzn <ide_0109@outlook.jp>
2021-03-01 23:16:21 +0900

    bug fix color

--- R/deseq2_tree.R
+++ R/deseq2_tree.R
@@ -22,6 +22,14 @@
 #'
 deseq2_tree <- function(physeq, deseq2_results, level = c("Kingdom", "Phylum",
     "Class", "Order", "Family", "Genus", "Species"), alpha = 0.05, sample_annotation = "Status", colors = nyankocolors) {
+
+    colors <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33",
+                "#A65628", "#F781BF", "#66C2A5", "#FC8D62", "#8DA0CB", "#E78AC3", "#A6D854",
+                "#FFD92F", "#E5C494", "#B3B3B3", "#8DD3C7", "#FFFFB3", "#BEBADA", "#FB8072",
+                "#80B1D3", "#FDB462", "#B3DE69", "#FCCDE5", "#D9D9D9", "#BC80BD", "#CCEBC5",
+                "#FFED6F", "#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C", "#FB9A99", "#E31A1C",
+                "#FDBF6F", "#FF7F00", "#CAB2D6", "#6A3D9A", "#FFFF99", "#B15928")
+
     sig_asv <- deseq2_results %>% as.data.frame() %>% dplyr::filter(padj < alpha)
 
     sig_tax <- physeq %>% tax_table() %>% as.data.frame() %>% rownames_to_column("ASV") %>%
--- R/microbiome_barplot.R
+++ R/microbiome_barplot.R
@@ -27,7 +27,14 @@
 #'
 microbiome_barplot <- function(physeq, level = c("Kingdom", "Phylum", "Class",
     "Order", "Family", "Genus", "Species"), plot_category, plot_percent = 10,
-    threshold = "max", na_str = c("unidentified", "uncultured"), colors = nyankocolors) {
+    threshold = "max", na_str = c("unidentified", "uncultured")) {
+
+    colors <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33",
+                      "#A65628", "#F781BF", "#66C2A5", "#FC8D62", "#8DA0CB", "#E78AC3", "#A6D854",
+                      "#FFD92F", "#E5C494", "#B3B3B3", "#8DD3C7", "#FFFFB3", "#BEBADA", "#FB8072",
+                      "#80B1D3", "#FDB462", "#B3DE69", "#FCCDE5", "#D9D9D9", "#BC80BD", "#CCEBC5",
+                      "#FFED6F", "#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C", "#FB9A99", "#E31A1C",
+                      "#FDBF6F", "#FF7F00", "#CAB2D6", "#6A3D9A", "#FFFF99", "#B15928")
 
     agg_phylo <- speedyseq::tax_glom(physeq, level, NArm = F)
     agg_phylo_rel <- transform_sample_counts(agg_phylo, function(x) 100 * x/sum(x))
