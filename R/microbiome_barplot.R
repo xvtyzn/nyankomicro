7055c6162a7d20b8859b1648a040db7bb295a2af
xvtyzn <ide_0109@outlook.jp>
2021-08-04 23:56:45 +0900

    refactoring

--- R/microbiome_barplot.R
+++ R/microbiome_barplot.R
@@ -29,65 +29,8 @@ microbiome_barplot <- function(physeq, level = c("Kingdom", "Phylum", "Class",
     "Order", "Family", "Genus", "Species"), plot_category, plot_percent = 10,
     threshold = "max", na_str = c("unidentified", "uncultured")) {
 
-    agg_phylo <- speedyseq::tax_glom(physeq, level, NArm = F)
-    agg_phylo_rel <- transform_sample_counts(agg_phylo, function(x) 100 * x/sum(x))
-    level_tax <- tax_table(agg_phylo_rel)[, level] %>% as.data.frame() %>% rownames_to_column("ASV")
-    level_tax[is.na(level_tax)] <- "Undetermined"
-
-    taxotu_table <- otu_table(agg_phylo_rel) %>% as.data.frame() %>% rownames_to_column("ASV") %>%
-        left_join(level_tax, by = ("ASV" = "ASV")) %>% select(-ASV) %>% group_by(across(all_of(level))) %>%
-        summarise_all(sum) %>% remove_rownames() %>% column_to_rownames(level)
-    taxotu_table$mean <- apply(taxotu_table, 1, mean)
-
-    if (threshold == "min") {
-        taxotu_table$threshold <- apply(taxotu_table, 1, min)
-    } else if (threshold == "max") {
-        taxotu_table$threshold <- apply(taxotu_table, 1, max)
-    } else if (threshold == "mean") {
-        taxotu_table$threshold <- apply(taxotu_table, 1, mean)
-    } else {
-        warning("You have not set function option.
-            This time, we will show a taxa of at least N%.\n")
-        taxotu_table$threshold <- apply(taxotu_table, 1, max)
-    }
-
-    undetermined_str <- paste(c("Undetermined", na_str), collapse = "|")
-
-    undetermined <- taxotu_table %>%
-        rownames_to_column("domain") %>%
-        filter(grepl(undetermined_str, domain)) %>%
-        column_to_rownames("domain") %>%
-        colSums()
-
-    show_tax <- taxotu_table %>% rownames_to_column("domain") %>% filter(!grepl(undetermined_str, domain)) %>%
-        filter(threshold >= plot_percent) %>% column_to_rownames("domain") %>%
-        arrange(desc(mean))
-
-    unshow_tax <- taxotu_table %>% rownames_to_column("domain") %>% filter(!grepl(undetermined_str, domain)) %>%
-        filter(threshold < plot_percent) %>% column_to_rownames("domain") %>%
-        colSums()
-
-    all_list <- list(show_tax, undetermined, unshow_tax)
-    all_tax <- purrr::reduce(all_list, rbind)
-    n <- nrow(all_tax)
-    rownames(all_tax)[n-1] <- "Undetermined"
-    rownames(all_tax)[n] <- paste0("Others (<", plot_percent, "%)")
-
-    all_tax_table <- all_tax %>% select(-c(threshold,mean)) %>% t()
-
-    if (nrow(all_tax) > length(colors)) {
-        stop("You have exceeded the number of colors allowed.
-         Increase the percent value for thresholds.\n")
-    }
-
-    sample_status <- agg_phylo %>% sample_data() %>% as.matrix() %>% as.data.frame() %>%
-        rownames_to_column("Sample")
-
-    all_ggdata <- all_tax_table %>% as.data.frame() %>% rownames_to_column("Sample") %>%
-        left_join(sample_status, by = ("Sample" = "Sample")) %>% reshape2::melt()
-
-    taxa_data_uniq <- unique(all_ggdata$variable)
-    all_ggdata$Taxa <- factor(all_ggdata$variable, levels = rev(taxa_data_uniq))
+    all_ggdata <- microbiome_bardata(physeq, level, plot_category, plot_percent,
+                                     threshold, na_str)
     colors <- rev(colors[1:length(taxa_data_uniq)])
     colors[1:2] <- c("grey30", "grey")
 
